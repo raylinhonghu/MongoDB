d79206cb285c4e4f23645fb87d1e7aebcbafc9c4
raylinhonghu <raylinhonghu@gmail.com>
2018-02-10 18:59:51 -0800

    update operator in note.txt and examples

--- test/update_test.js
+++ test/update_test.js
@@ -19,8 +19,7 @@ describe('Updating records',function(){
   // MarioChar.update({})
   // MarioChar.findOneAndUpdate({},{})
 
-
-  it('Update a record in the database',function(done){
+  it('Update a record weight in the database',function(done){
 
     // asychronous
     MarioChar.findOneAndUpdate({weight:33}).then(function(){
@@ -32,12 +31,26 @@ describe('Updating records',function(){
 
   });
 
-  it('Update a record in the database',function(done){
+  it('Update a record name',function(done){
 
     // asychronous
     MarioChar.findOneAndUpdate({name:'Mario'},{name:'Luigi'}).then(function(){
       MarioChar.findOne({_id: char._id}).then(function(result){
         assert(result.name === 'Luigi');
+        done();
+      });
+    });
+
+  });
+
+  // https://docs.mongodb.com/manual/reference/operator/update/
+  // Update operator
+  it('Update all weight by 1',function(done){
+
+    // asychronous
+    MarioChar.update({}, { $inc: {weight: 1}}).then(function(){
+      MarioChar.findOne({name:'Mario'}).then(function(record){
+        assert(record.weight === char.weight + 1);
         done();
       });
     });
